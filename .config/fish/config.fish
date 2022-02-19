71664fed1b5566ef2de9a0898b57add585d23a52
ChrisOtt0 <sophusott@gmail.com>
2022-02-20 00:07:56 +0100

    fish & neofetch configured

---
+++ .config/fish/config.fish
@@ -0,0 +1,9 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+neofetch --disable gpu --color_blocks off --colors 45 120 248 45 253
+
+# Aliases
+alias ls='ls -a'
+alias neofetch='neofetch --disable gpu --colors_blocks off --colors 45 120 248 45 253'
--- autosetup.sh
+++ autosetup.sh
@@ -1,7 +1,9 @@
 sudo pacman -S \
 	discord \
+	fish \
 	gtop \
 	htop \
+	neofetch \
 	neovim \
 	pipewire \
 	pipewire-jack \
@@ -28,13 +30,21 @@ sudo cp -r ~/Documents/dotfiles_qtile/backgrounds /usr/share/
 # setup lightdm
 sudo cp -r ~/Documents/dotfiles_qtile/lightdm/slick-greeter.conf /etc/lightdm/
 
+# setup fish
+echo /usr/local/bin/fish | sudo tee -a /etc/shells
+sudo chsh -s /bin/fish
+curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
+omf update
+set -g EDITOR nvim
+set -g BROWSER librewolf
+exit
+
+# setup .config folder
+cp -r ~/Documents/dotfiles_qtile/.config/ ~/
+
 # setup gtk & icon theme
 cp ~/Documents/dotfiles_qtile/gtkrc-2.0 ~/
 mkdir ~/.local/share/icons
 cp ~/Documents/dotfiles_qtile/icons ~/.local/share/icons
 mkdir ~/.themes
 cp ~/Documents/dotfiles_qtile/.themes ~/
-
-# setup .config folder
-cp -r ~/Documents/dotfiles_qtile/.config/ ~/
-
